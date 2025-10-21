cc510796e441905ca9e3283030e0f00af7e302ad
Hidaya Mohamed <hidayamohamed002@gmail.com>
2025-10-21 08:07:23 +0300

    Complete lab

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+function About() {
+  return (
+      <>
+          <header>
+              <NavBar />
+          </header>
+          <main>
+              <h1>This my about component</h1>
+          </main>
+      </>
+  )
+}
+
+export default About
